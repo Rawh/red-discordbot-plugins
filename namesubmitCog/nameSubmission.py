86968ddb5e4a4f5f66da397c4430729307f616b1
Rawh <wavanderpoel@gmail.com>
2019-10-18 00:27:18 +0200

    added role-id for is_mod and is_allowed; added namechange; added more checks and messages about invalid options;

--- namesubmitCog/nameSubmission.py
+++ namesubmitCog/nameSubmission.py
@@ -56,8 +56,10 @@ class nameSubmission(commands.Cog):
 		groups = user.roles
 		allowed = False
 		for group in groups:
-			# if group.name.lower() == "name submitter moderator":
-			if group.name.lower() == "the nemoderators":
+			if (group.name.lower() == "the nemo") or (group.name.lower() == "the nemoderators"):
+				allowed = True
+				break
+			if (group.id == 271701295952297984) or (group.id == 553349134783086592):
 				allowed = True
 				break
 			
@@ -77,8 +79,17 @@ class nameSubmission(commands.Cog):
 		groups = user.roles
 		allowed = False
 		for group in groups:
-			# if group.name.lower() == "name submitter":
-			if group.name.lower() == "patron of the nemo":
+			if (group.name.lower() == "patron of the nemo") or (group.name.lower() == "bit parts and recurring characters"):
+				allowed = True
+				break
+			if (group.id == 276803337100197888) or (group.id == 628688754940575774):
+				allowed = True
+				break
+			#Mods added also
+			if (group.name.lower() == "the nemo") or (group.name.lower() == "the nemoderators"):
+				allowed = True
+				break
+			if (group.id == 271701295952297984) or (group.id == 553349134783086592):
 				allowed = True
 				break
 			
@@ -95,13 +106,26 @@ class nameSubmission(commands.Cog):
 		name = " ".join(name)
 		name = name.lower()
 		
+		if name is None:
+			await ctx.send("Invalid name")
+			return False
+		if name == "":
+			await ctx.send("Invalid name")
+			return False
+		
 		currentNames = await self.config.GAMES()
 		
 		if not game.lower() in currentNames:
 			await ctx.send("This game does not exist")
 		else:
+			
+			for nameOld in currentNames[game.lower()]:
+				if currentNames[game.lower()][nameOld][1] == ctx.message.author.id:
+					await ctx.send("You have already submited a name for this game")
+					return False
+			
 			if not name in currentNames[game.lower()]:
-				currentNames[game.lower()][name] = False
+				currentNames[game.lower()][name] = [False,ctx.message.author.id]
 				await self.config.GAMES.set(currentNames)
 				await ctx.send("Name submited")
 			else:
@@ -123,17 +147,51 @@ class nameSubmission(commands.Cog):
 			keys = list(names.keys())
 			random_name = ""
 			for key in keys:
-				if not names[key]:
+				if not names[key][0]:
 					random_name = key
 					break
 			
 			if not random_name == "":
-				currentNames[game.lower()][random_name] = True
+				currentNames[game.lower()][random_name] = [True,currentNames[game.lower()][random_name][1]]
 				await self.config.GAMES.set(currentNames)
 				await ctx.send(random_name)
 			else:
 				await ctx.send("Unable to get an unused name")
-
+	
+	@commands.command(pass_context=True, brief="Changes a name", name='namechange')	
+	@commands.check(is_allowedToSubmit)
+	async def namechange(self, ctx: commands.Context,game:str, *name):
+		"""Changes your submited name"""
+		
+		name = " ".join(name)
+		name = name.lower()
+		
+		if name is None:
+			await ctx.send("Invalid name")
+			return False
+		if name == "":
+			await ctx.send("Invalid name")
+			return False
+		
+		currentNames = await self.config.GAMES()
+		
+		if not game.lower() in currentNames:
+			await ctx.send("This game does not exist")
+		else:
+			keyToRemove = None
+			for nameOld in currentNames[game.lower()]:
+				if currentNames[game.lower()][nameOld][1] == ctx.message.author.id:
+					keyToRemove = nameOld
+					break
+				
+			if not keyToRemove is None:
+				currentNames[game.lower()][name] = [currentNames[game.lower()][nameOld][0],ctx.message.author.id]
+				del currentNames[game.lower()][nameOld]
+				await self.config.GAMES.set(currentNames)
+				await ctx.send("Name changed")
+			else:
+				await ctx.send("You have not submitted any name for this game")
+	
 	@commands.command(pass_context=True, brief="Clears all names from game", name='nameclear')		
 	@commands.check(is_mod)
 	async def nameclear(self, ctx: commands.Context,game:str):
@@ -155,15 +213,25 @@ class nameSubmission(commands.Cog):
 		name = " ".join(name)
 		name = name.lower()
 		
+		if name is None:
+			await ctx.send("Invalid name")
+			return False
+		if name == "":
+			await ctx.send("Invalid name")
+			return False
+		
 		currentNames = await self.config.GAMES()
 		
 		if not game.lower() in currentNames:
 			await ctx.send("This game does not exist")
 		else:
 			if name in currentNames[game.lower()]:
-				currentNames[game.lower()][name] = False
-				await self.config.GAMES.set(currentNames)
-				await ctx.send("Name reset")
+				if not currentNames[game.lower()][name][0]:
+					await ctx.send("Name has not been used yet")
+				else:
+					currentNames[game.lower()][name] = [False,currentNames[game.lower()][name][1]]
+					await self.config.GAMES.set(currentNames)
+					await ctx.send("Name reset")
 			else:
 				await ctx.send("This name does not exist")
 
