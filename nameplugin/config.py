ce75f483088b7a557337834938025e769d5d023a
Rawh <wavanderpoel@gmail.com>
2019-10-28 18:35:56 +0100

    add twitch subs to permission list

--- nameplugin/config.py
+++ nameplugin/config.py
@@ -1,3 +1,3 @@
 custom_user_roles = ['The Nemo', 'The Nemoderators',
-                     'Patron of the Nemo', 'Bit Parts and Recurring Characters']
+                     'Patron of the Nemo', 'Bit Parts and Recurring Characters','Twitch Subscribers']
 custom_admin_roles = ['The Nemo', 'The Nemoderators']
