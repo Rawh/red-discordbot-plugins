65ad1cebbbac124e502017df93af034777f84741
Rawh <10486655+Rawh@users.noreply.github.com>
2020-04-16 00:16:50 +0200

    Update config.py

--- custom_name_submit/config.py
+++ custom_name_submit/config.py
@@ -1,4 +1,4 @@
 custom_user_roles = ['The Nemo', 'The Nemoderators',
-                     'Patron of the Nemo', 'Bit Parts and Recurring Characters']
+                     'Patron of the Nemo', 'Bit Parts and Recurring Characters', 'Twitch Subscribers']
 custom_admin_roles = ['The Nemo', 'The Nemoderators']
 whitelisted_channels = ['name-admin', 'name-management']
